7a7791fb8d65bfa36988811c622aedd8338ba20c
ryanboris <ryan.boris@gmail.com>
2019-05-16 04:01:55 -0400

    Builds out the logic for users and authentication based routes

--- client/src/containers/App/App.js
+++ client/src/containers/App/App.js
@@ -1,11 +1,34 @@
 import React from 'react'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
 
-const App = () => {
-  return (
+import Navigation from '../../views/Navigation'
+import LandingPage from '../../views/Landing'
+import SignUpPage from '../../components/SignUp/index'
+import SignInPage from '../../components/SignIn/index'
+import PasswordForgetPage from '../../components/PasswordForget/index'
+import HomePage from '../../views/Home'
+import AccountPage from '../../views/Account'
+import AdminPage from '../../views/Admin'
+
+import * as ROUTES from '../../constants/routes'
+import { withAuthentication } from '../Sessions'
+
+const App = () => (
+  <Router>
     <div>
-      Sanity Test!
-    </div>
-  )
-}
+      <Navigation />
 
-export default App
+      <hr />
+
+      <Route exact path={ROUTES.LANDING} component={LandingPage} />
+      <Route path={ROUTES.SIGN_UP} component={SignUpPage} />
+      <Route path={ROUTES.SIGN_IN} component={SignInPage} />
+      <Route path={ROUTES.PASSWORD_FORGET} component={PasswordForgetPage} />
+      <Route path={ROUTES.HOME} component={HomePage} />
+      <Route path={ROUTES.ACCOUNT} component={AccountPage} />
+      <Route path={ROUTES.ADMIN} component={AdminPage} />
+    </div>
+  </Router>
+)
+
+export default withAuthentication(App)
