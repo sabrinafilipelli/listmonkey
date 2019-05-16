21f037515d815f6fcf23d35d2e303e25fae1b07f
ryanboris <ryan.boris@gmail.com>
2019-05-16 02:48:59 -0400

    Generates a new firebase application instance and establishes relevant auth Firebase APIs

---
+++ client/src/containers/Firebase/firebase.js
@@ -0,0 +1,147 @@
+import app from 'firebase/app'
+import 'firebase/auth'
+import 'firebase/database'
+import { config } from './config'
+
+/**
+ * @class Firebase
+ * @description Creates an instance of a firebase application and provides all of the require methods for initializing, generating, and interacting with required APIs
+ */
+
+class Firebase {
+  constructor() {
+    // Main initialization of firebase
+
+    app.initializeApp(config)
+
+    // Helper - Database && Server Connectivity
+
+    this.serverValue = app.database.ServerValue
+
+    // Helper - Email Authentication Initialization
+
+    this.emailAuthProvider = app.auth.EmailAuthProvider
+
+    // Firebase auth and database APIs
+
+    this.auth = app.auth()
+    this.db = app.database()
+
+    // Instantiates a new OAuth provider for a given social app
+
+    this.googleProvider = new app.auth.GoogleAuthProvider()
+    this.facebookProvider = new app.auth.FacebookAuthProvider()
+    this.twitterProvider = new app.auth.TwitterAuthProvider()
+    this.githubProvider = new app.auth.GithubAuthProvider()
+  }
+
+  // Authorization API Methods -- All of these class methods will           handle the invocation of a function that is from the Firebase        API
+
+  /**
+   * @method doCreateUserWithEmailandPassword
+   * @param {string} email
+   * @param {string} password
+   * @description Creates a new user and stores the user's email and hashed password
+   *
+   */
+
+  doCreateUserWithEmailAndPassword = (email, password) =>
+    this.auth.createUserWithEmailAndPassword(email, password)
+
+  /**
+   * @method doSignInWithEmailAndPassword
+   * @param {string} email
+   * @param {string} password
+   * @description Will sign in an existing user and also handles all                  the hashing and encoding as it invokes a Firebase                   API function.
+   *
+   */
+
+  doSignInWithEmailAndPassword = (email, password) =>
+    this.auth.signInWithEmailAndPassword(email, password)
+
+  /**
+   * @method doSignInWithGoogle
+   * @method doSignInWithFacebook
+   * @method doSignInWithTwitter
+   * @method doSignInWithGithub
+   * @description Invokes the corresponding Firebase OAuth2.0 Sign-in                 APIs
+   *
+   */
+
+  doSignInWithGoogle = () => this.auth.signInWithPopup(this.googleProvider)
+
+  doSignInWithFacebook = () => this.auth.signInWithPopup(this.facebookProvider)
+
+  doSignInWithTwitter = () => this.auth.signInWithPopup(this.twitterProvider)
+
+  doSignInWithGithub = () => this.auth.signInWithPopup(this.githubProvider)
+
+  /**
+   * @method doSignOut
+   * @description Firebase API sign out function.
+   */
+
+  doSignOut = () => this.auth.signOut()
+
+  /**
+   * @method doPasswordReset
+   * @method doPasswordUpdate
+   * @description These Firebase API functions allow users to reset                   and update their existing passwords.
+   *
+   */
+
+  doPasswordReset = email => this.auth.sendPasswordResetEmail(email)
+
+  doPasswordUpdate = password => this.auth.currentUser.updatePassword(password)
+
+  /**
+   * @method doSendEmailVerification
+   * @description When a new user signs up with an email and password, this Firebase API will use a second-factor email based verification to authorize the user.
+   */
+
+  doSendEmailVerification = () =>
+    this.auth.currentUser.sendEmailVerification({
+      url: 'https://chore-monkey-app.firebaseapp.com/'
+    })
+
+  // Helper method which merges the Firebase Authentication Storage Container with the Realtime Database API
+
+  onAuthUserListener = (next, fallback) =>
+    this.auth.onAuthStateChanged(authUser => {
+      if (authUser) {
+        this.user(authUser.uid)
+          .once('value')
+          .then(snapshot => {
+            const dbUser = snapshot.val()
+
+            // default user is created with no admin rights
+            if (!dbUser.roles) {
+              dbUser.roles = {}
+            }
+
+            // merges authorized admin user with dbUser
+            authUser = {
+              uid: authUser.uid,
+              email: authUser.email,
+              emailVerified: authUser.emailVerified,
+              providerData: authUser.providerData,
+              ...dbUser
+            }
+
+            next(authUser)
+          })
+      } else {
+        fallback()
+      }
+    })
+
+  // User API Methods
+
+  // Returns a user by id
+  user = uid => this.db.ref(`users/${uid}`)
+
+  // Returns all users
+  users = () => this.db.ref('users')
+}
+
+export default Firebase
