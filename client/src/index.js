ec7d05b563aab22bc3ee1f17982933b189807b93
SabrinaGear <sabrinagear.ft@gmail.com>
2019-05-21 10:31:42 -0500

    navigation changes, landing page starting, router, routing transitions

--- client/src/index.js
+++ client/src/index.js
@@ -1,12 +1,16 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import App from './containers/App'
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter } from "react-router-dom";
+import App from "./containers/App";
+import styles from "./styles/index.css";
 
-import Firebase, { FirebaseContext } from './containers/Firebase'
+import Firebase, { FirebaseContext } from "./containers/Firebase";
 
 ReactDOM.render(
-  <FirebaseContext.Provider value={new Firebase()}>
-    <App />
-  </FirebaseContext.Provider>,
-  document.getElementById('root')
-)
+  <BrowserRouter>
+    <FirebaseContext.Provider value={new Firebase()}>
+      <App />
+    </FirebaseContext.Provider>
+  </BrowserRouter>,
+  document.getElementById("root")
+);
