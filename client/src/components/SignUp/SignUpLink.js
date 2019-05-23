be23a1ab922d80394ac6aab43c84fb1da47ac7d3
ryanboris <ryan.boris@gmail.com>
2019-05-23 02:53:33 -0400

    removes unusued junk

--- client/src/components/SignUp/SignUpLink.js
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-
-export const SignUpLink = () => (
-  <p>
-    Don't have an account? <Link to="/signup">Sign Up</Link>
-  </p>
-)
